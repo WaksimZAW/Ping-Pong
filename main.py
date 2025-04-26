2db8c927acbdc26208453f7cba7b2f2a4355fd0b
WaksimZAW <149604348+WaksimZAW@users.noreply.github.com>
2025-04-26 15:03:07 +0200

    Добавили ракетки для игроков.

--- main.py
+++ main.py
@@ -23,6 +23,22 @@ class GameSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+
+#Класс для игрока, двойное наследование
+class Player(GameSprite):
+    def __init__(self, img, x, y, size, speed, key_up, key_down):
+        super().__init__(img, x, y, size, speed)
+        self.key_up = key_up
+        self.key_down = key_down
+
+    def update(self):
+        keys = key.get_pressed()
+
+        if keys[self.key_up] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[self.key_down] and self.rect.y < win_size[1] - self.size[1] - 5:
+            self.rect.y += self.speed
+
 #Настройка игровой сцены
 win_size = (700, 500)
 window = display.set_mode(win_size)
@@ -35,6 +51,11 @@ game = True
 finish = False
 clock = time.Clock()
 FPS = 60
+racket_img = "racket.png"
+
+player_1 = Player(racket_img, 30, 200, (40, 140), 4, K_UP, K_DOWN)
+player_2 = Player(racket_img, 620, 200, (40, 140), 4, K_w, K_s)
+
 
 #Игровой цикл
 while game:
@@ -44,6 +65,12 @@ while game:
 
     if not finish:
         window.blit(background, (0, 0))
+
+        player_1.update()
+        player_2.update()
+
+        player_1.reset()
+        player_2.reset()
 
     display.update()
     clock.tick(FPS)
