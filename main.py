c755097e4979bff4fda3e81acd9c10cee64214d8
WaksimZAW <149604348+WaksimZAW@users.noreply.github.com>
2025-05-03 14:38:10 +0200

    Добавили счёт игроков и надписи для голов.

--- main.py
+++ main.py
@@ -51,6 +51,12 @@ class Ball(GameSprite):
         self.rect.x += self.speed_x
         self.rect.y += self.speed_y
 
+    def bounce_x(self):
+        self.speed_x *= -1
+
+    def bounce_y(self):
+        self.speed_y *= -1
+
 
 #Настройка игровой сцены
 win_size = (700, 500)
@@ -65,6 +71,8 @@ finish = False
 clock = time.Clock()
 FPS = 60
 racket_img = "racket.png"
+score_1 = 0
+score_2 = 0
 
 #Создание игроков
 player_1 = Player(racket_img, 30, 200, (40, 140), 4, K_UP, K_DOWN)
@@ -75,10 +83,13 @@ ball_img = "ball.png"
 ball = Ball(ball_img, 300, 300, (40, 40), 0, 3, 3)
 # Надписи игры
 font.init()
-my_font = font.SysFont("verdana", 24)
+my_font = font.SysFont("verdana", 20, bold = True)
 endgame_font = font.SysFont("verdana", 76)
 lose_1 = endgame_font.render("Игрок 1 проиграл!", True, (180, 0, 0))
 lose_2 = endgame_font.render("Игрок 2 проиграл!", True, (180, 0, 0))
+goal_font = font.SysFont("verdana", 20)
+goal_1 = goal_font.render("Игрок 1 забивает!", True, (0, 0, 255))
+goal_2 = goal_font.render("Игрок 2 забивает!", True, (255, 0, 0))
 
 
 #Игровой цикл
@@ -89,6 +100,11 @@ while game:
 
     if not finish:
         window.blit(background, (0, 0))
+        score_1_text = my_font.render(f"Счёт 1: {score_1}", True, (0, 0 , 0))
+        score_2_text = my_font.render(f"Счёт 2: {score_2}", True, (0, 0 , 0))
+        window.blit(score_1_text, (10, 15))
+        window.blit(score_2_text, (win_size[0]-115, 15))
+
 
         player_1.update()
         player_2.update()
